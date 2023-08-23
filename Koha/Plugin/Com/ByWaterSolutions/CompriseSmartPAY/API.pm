71d776330a07337047f96c284b1edc8a1cff4bd0
Kyle M Hall <kyle@bywatersolutions.com>
2023-08-23 18:58:19 +0000

    Implement send transaction and query result

--- Koha/Plugin/Com/ByWaterSolutions/CompriseSmartPAY/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/CompriseSmartPAY/API.pm
@@ -19,8 +19,10 @@ use Modern::Perl;
 
 use Mojo::Base 'Mojolicious::Controller';
 
+use URI::Escape;
 use HTTP::Request::Common;
 use JSON qw( to_json );
+use Mojo::JSON qw( decode_json );
 use YAML qw( Load );
 
 use C4::Context;
@@ -34,19 +36,20 @@ use C4::Context;
 =cut
 
 sub get_debits {
-    warn "Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::get_debits";
+    warn
+      "Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::get_debits";
     my $c = shift->openapi->valid_input or return;
 
     my $accountline_ids = $c->param('accountlines');
-    warn "IDS: $accountline_ids";
 
-    my @ids = split(/,/, $accountline_ids );
+    my @ids = split( /,/, $accountline_ids );
 
-    my $accountlines = Koha::Account::Lines->search({ accountlines_id => \@ids } )->unblessed;
+    my $accountlines =
+      Koha::Account::Lines->search( { accountlines_id => \@ids } )->unblessed;
 
     my $total = 0;
     $total += $_->{amountoutstanding} for @$accountlines;
-    $total = Koha::Number::Price->new( $total )->format_for_editing;
+    $total = Koha::Number::Price->new($total)->format_for_editing;
 
     my $json = to_json( { total => $total, accountlines => $accountlines } );
 
@@ -54,17 +57,20 @@ sub get_debits {
 }
 
 sub send_transaction {
-    warn "Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::send_transaction";
+    warn
+"Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::send_transaction";
     my $c = shift->openapi->valid_input or return;
 
     my $accountline_ids = $c->param('accountlines');
-    my $amount = $c->param('amount') / 100; # Periods asplode things, so we recieve the amount in cents
-    warn "AMOUNT: $amount";
-    warn "IDS: $accountline_ids";
+    my $amount          = $c->param('amount') /
+      100;    # Periods asplode things, so we recieve the amount in cents
 
-    my @ids = split(/,/, $accountline_ids );
+    # Look up the patron from the first accountline
+    my ($id)        = split( /,/, $accountline_ids );
+    my $accountline = Koha::Account::Lines->find($id);
+    my $cardnumber  = $accountline->patron->cardnumber;
 
-    my $self = Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY->new({});
+    my $self = Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY->new( {} );
 
     my $CustomerId      = $self->retrieve_data('CustomerId');
     my $CustomerName    = $self->retrieve_data('CustomerName');
@@ -75,30 +81,130 @@ sub send_transaction {
     my $ServerAddress   = $self->retrieve_data('ServerAddress');
     my $RegisterMapping = $self->retrieve_data('RegisterMapping');
 
-    my $mapping = Load( $RegisterMapping );
+    my $mapping = Load($RegisterMapping);
 
     my $register_id = C4::Context->userenv->{register_id};
 
     my $terminal_id = $mapping->{$register_id};
-    return $c->render( status => 404, format => "json", json => { error => "TERM", description => "Terminal mapping for register $register_id found" } ) unless $terminal_id;
+    return $c->render(
+        status => 404,
+        format => "json",
+        json   => {
+            error       => "TERM",
+            description => "Terminal mapping for register $register_id found"
+        }
+    ) unless $terminal_id;
 
-    my $json = to_json( { amount => $amount });
+    my $header = HTTP::Headers->new;
+    $header->header( 'Content-Type' => 'application/json; charset=utf-8' );
+    $header->header( 'accept'       => 'application/json' );
+
+    my $uuid = generate_uuid();
+    my $content = {
+        CustomerID => $CustomerId,
+        TerminalID => $terminal_id,
+        UserName   => $UserName,
+        Password   => $Password,
+        Amount     => $amount,
+        TrackId    => $uuid,
+        PatronId   => $cardnumber,
+        Detail     => "Payment for fees",
+    };
+    my $params = join('&', map { "$_=" . uri_escape( $content->{$_} ) } keys %$content );
+
+    my $url = "$ServerAddress?SendTransaction&$params";
+
+    my $ua      = LWP::UserAgent->new;
+
+    my $request = HTTP::Request::Common::GET(
+        $url,
+        Header  => $header,
+    );
+    my $response = $ua->request($request);
+
+    unless ( $response->is_success ) {
+        warn "Comprise SmartPAY response indicates failure: "
+          . $response->status_line;
+    }
+
+    my $json = $response->decoded_content;
 
     return $c->render( status => 200, format => "json", text => $json );
 }
 
-sub amd_callback {
-    warn "Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::amd_callback";
+sub query_result {
+    warn
+"Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY::API::query_result";
     my $c = shift->openapi->valid_input or return;
-    return try {
 
-        my $message_id = $c->validation->param('message_id');
-        return $c->render( status => 204, text => q{} );
+    my $track_number = $c->param('tracknumber');
+
+    my $self = Koha::Plugin::Com::ByWaterSolutions::CompriseSmartPAY->new( {} );
+
+    my $CustomerId      = $self->retrieve_data('CustomerId');
+    my $UserName        = $self->retrieve_data('UserName');
+    my $Password        = $self->retrieve_data('Password');
+    my $ServerAddress   = $self->retrieve_data('ServerAddress');
+    my $RegisterMapping = $self->retrieve_data('RegisterMapping');
+
+    my $mapping = Load($RegisterMapping);
+
+    my $register_id = C4::Context->userenv->{register_id};
+
+    my $terminal_id = $mapping->{$register_id};
+    return $c->render(
+        status => 404,
+        format => "json",
+        json   => {
+            error       => "TERM",
+            description => "Terminal mapping for register $register_id found"
+        }
+    ) unless $terminal_id;
+
+    my $header = HTTP::Headers->new;
+    $header->header( 'Content-Type' => 'application/json; charset=utf-8' );
+    $header->header( 'accept'       => 'application/json' );
+
+    my $uuid = generate_uuid();
+    my $content = {
+        CustomerID => $CustomerId,
+        TerminalID => $terminal_id,
+        UserName   => $UserName,
+        Password   => $Password,
+        TrackNo    => $track_number,
+    };
+    my $params = join('&', map { "$_=" . uri_escape( $content->{$_} ) } keys %$content );
+
+    my $url = "$ServerAddress?QueryResult&$params";
+
+    my $ua      = LWP::UserAgent->new;
+
+    my $request = HTTP::Request::Common::GET(
+        $url,
+        Header  => $header,
+    );
+    my $response = $ua->request($request);
+
+    unless ( $response->is_success ) {
+        warn "Comprise SmartPAY response indicates failure: "
+          . $response->status_line;
     }
-    catch {
-        warn "CAUGHT UNHANDLED ERROR: $_";
-        $c->unhandled_exception($_);
+
+    my $json = $response->decoded_content;
+
+    return $c->render( status => 200, format => "json", text => $json );
+}
+
+sub generate_uuid {
+    my $uuid = '';
+    for ( 1 .. 4 ) {
+        $uuid .= pack 'I', int( rand( 2**32 ) );
     }
+
+    substr $uuid, 6, 1, chr( ord( substr( $uuid, 6, 1 ) ) & 0x0f | 0x40 );
+
+    return join '', map { unpack 'H*', $_ }
+      map { substr $uuid, 0, $_, '' } ( 4, 2, 2, 2, 6 );
 }
 
 1;
